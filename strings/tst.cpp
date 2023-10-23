aca289d1691a6fd03c7143f7cbae815a1a3003de
tahaekko <tahaexo@gmail.com>
2023-10-23 12:01:07 +0100

    str func

--- strings/tst.cpp
+++ strings/tst.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+using namespace std;
+
 int main( int ac, char **av)
 {
-	std::string str = NULL;
+	std::string str = "   Hello world Hello ";
+
+	cout << str.length() << endl;
 
 	if (str.empty())
-		std::cout << "here we are";
+		std::cout << "here we are" << endl;
+	//func discover
+	size_t found = str.find("Hello");
+	cout << found << endl;
+	string needl("Hello");
+	str = str.erase(found, needl.length());
+
+
+	cout << str.length() << endl;
 }
